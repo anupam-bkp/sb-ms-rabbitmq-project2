3c37d4fd2f956c25cd600b51256c64a1bdca2277
anupam <anupam.nitdgp14@gmail.com>
2025-02-16 23:40:21 +0530

    Created consumer stock service to consume data published on order_stock_queue.

---
+++ stock-service/src/main/java/com/learner/stockservice/dto/OrderEvent.java
@@ -0,0 +1,50 @@
+package com.learner.stockservice.dto;
+
+public class OrderEvent {
+
+    private String status; //pending, progress, completed
+    private String message;
+    private Order order;
+
+    public OrderEvent() {
+    }
+
+    public OrderEvent(String status, String message, Order order) {
+        this.status = status;
+        this.message = message;
+        this.order = order;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderEvent{" +
+                "status='" + status + '\'' +
+                ", message='" + message + '\'' +
+                ", order=" + order +
+                '}';
+    }
+}
